aaa5371c4d22380dc3e8e945d15f5bbd13c55d92
sulasoft <davids@sulasoft.com>
2022-07-19 20:32:25 -0400

    make it better

--- prueba_scrapy.py
+++ prueba_scrapy.py
@@ -125,19 +125,30 @@ try:
 		
 																	
 		try:
+			print("Primer intento")
 			business_websites = business_drive.find_element(By.XPATH, '//*[@id="QA0Szd"]/div/div/div[1]/div[3]/div/div[1]/div/div/div[2]/div[7]/div[8]/div[2]/div/div[1]/a').get_attribute("href")
 		except:
+			print("Error...")
 			try:
+				print("Segundo intento")
 				business_websites = business_drive.find_element(By.XPATH, '//*[@id="QA0Szd"]/div/div/div[1]/div[3]/div/div[1]/div/div/div[2]/div[7]/div[4]/div[2]/div/div[1]/a').get_attribute("href")
 			except:
+				print("Error...")
 				try:
+					print("Tercer intento")
 					business_websites = business_drive.find_element(By.XPATH, '//*[@id="QA0Szd"]/div/div/div[1]/div[3]/div/div[1]/div/div/div[2]/div[7]/div[5]/div[2]/div/div[1]/a').get_attribute("href")
 				except:
-					try:                                           
+					print("Error...")
+					try:
+						print("Cuarto intento")                                           
 						business_websites = business_drive.find_element(By.XPATH, '//*[@id="QA0Szd"]/div/div/div[1]/div[3]/div/div[1]/div/div/div[2]/div[7]/div[7]/div[2]/div/div[1]/a').get_attribute("href")
 					except:
-						business_websites = business_drive.find_element(By.XPATH, '//*[@id="QA0Szd"]/div/div/div[1]/div[3]/div/div[1]/div/div/div[2]/div[7]/div[6]/div[2]/div/div[1]/a').get_attribute("href")
-																   
+						print("Error...")
+						try:
+							print("Quinto intento")
+							business_websites = business_drive.find_element(By.XPATH, '//*[@id="QA0Szd"]/div/div/div[1]/div[3]/div/div[1]/div/div/div[2]/div[7]/div[6]/div[2]/div/div[1]/a').get_attribute("href")
+						except:
+							print("Error...")							   
 	#	if len(business_websites) == 0:
 	#		business_websites = driver.find_element(By.CLASS_NAME, ' wV5uyc')
 	#		business_websites = business.find_element(By.CLASS_NAME, 'VfPpkd-dgl2Hf-ppHlrf-sM5MNb').get_attribute("href")
@@ -202,11 +213,8 @@ try:
 	business = pd.DataFrame(list_business, columns = ['Name', 'Address', 'Email'])
 	business.to_csv('data.csv')
 	business.to_excel('data.xlsx', sheet_name='data')
-
-	
 	
 	print(4)
-
 	
 	input("Ready, press a key")
 
